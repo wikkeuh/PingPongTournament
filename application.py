680958fea5c2e70bae1d5cebf62c649f4ad06f4d
Wim Das <wikkeuh@gmail.com>
2019-03-11 11:15:47 +0000

    made log-in case insenstive for username

--- application.py
+++ application.py
@@ -71,7 +71,7 @@ def login():
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE lower(username) = :username",
-                          username=string.lower(request.form.get("username")))
+                          username = str.lower(request.form.get("username")))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
